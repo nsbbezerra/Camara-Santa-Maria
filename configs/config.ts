04f15cfe611b03bc4388c9743cb2364f64e1e0e1
Natanael Bezerra <nsbbezerra@hotmail.com>
2021-10-26 10:14:07 -0300

    change url to server

--- configs/config.ts
+++ configs/config.ts
@@ -2,7 +2,7 @@ const producao = "https://vps34531.publiccloud.com.br";
 const devops = "http://localhost:3333";
 
 const config = {
-  default_url: devops,
+  default_url: producao,
 };
 
 export { config };
